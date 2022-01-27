ea01c9a7b2ba09e04dd3b851396dbf9f0ae49942
katarzyna wegrzynowicz <katarzyna.koch@gmail.com>
2022-01-27 09:47:04 -0600

    formatting in Body, index, and data

--- src/data.js
+++ src/data.js
@@ -1,67 +1,37 @@
 const navbarLinks = [
-    {
-        "category": "Product",
-        "links": [
-            "Overview",
-            "Pricing",
-            "Marketplace",
-            "Features",
-            "Integrations"
-        ]
-    },
-    {
-        "category": "Company",
-        "links": [
-            "About",
-            "Team",
-            "Blog",
-            "Careers"
-        ]
-    },
-    {
-        "category": "Connect",
-        "links": [
-            "Contact",
-            "Newsletter",
-            "LinkedIn"
-        ]
-    }
-]
+  {
+    category: "Product",
+    links: ["Overview", "Pricing", "Marketplace", "Features", "Integrations"],
+  },
+  {
+    category: "Company",
+    links: ["About", "Team", "Blog", "Careers"],
+  },
+  {
+    category: "Connect",
+    links: ["Contact", "Newsletter", "LinkedIn"],
+  },
+];
 
 const navbarProducts = [
-    {
-        "category": "Product",
-        "links": [
-            "Overview",
-            "Pricing",
-            "Marketplace",
-            "Features",
-            "Integrations"
-        ]
-    }
-]
+  {
+    category: "Product",
+    links: ["Overview", "Pricing", "Marketplace", "Features", "Integrations"],
+  },
+];
 
 const navbarContact = [
-    {
-        "category": "Company",
-        "links": [
-            "About",
-            "Team",
-            "Blog",
-            "Careers"
-        ]
-    }
-]
+  {
+    category: "Company",
+    links: ["About", "Team", "Blog", "Careers"],
+  },
+];
 
 const navbarConnect = [
-    {
-        "category": "Connect",
-        "links": [
-            "Contact",
-            "Newsletter",
-            "LinkedIn"
-        ]
-    }
-]
+  {
+    category: "Connect",
+    links: ["Contact", "Newsletter", "LinkedIn"],
+  },
+];
 
-export {navbarProducts, navbarLinks, navbarContact, navbarConnect}
+export { navbarProducts, navbarLinks, navbarContact, navbarConnect };
