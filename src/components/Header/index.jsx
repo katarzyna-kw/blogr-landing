d2ca4be8f36e879b158369d5d5eefebe96775c40
katarzyna wegrzynowicz <katarzyna.koch@gmail.com>
2022-01-28 10:35:51 -0600

    Header/Navbar styles for tablet/desktop

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,67 +1,69 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from '../Navbar'
-import desktopBg from '../../images/bg-pattern-intro-desktop.svg'
-import mobileBg from '../../images/bg-pattern-intro-mobile.svg'
-import logo from '../../images/logo.svg'
-import hamburger from '../../images/icon-hamburger.svg'
-import close from '../../images/icon-close.svg'
-import './Header.css';
+import React, { useState, useEffect } from "react";
+import Navbar from "../Navbar";
+import desktopBg from "../../images/bg-pattern-intro-desktop.svg";
+import mobileBg from "../../images/bg-pattern-intro-mobile.svg";
+import logo from "../../images/logo.svg";
+import hamburger from "../../images/icon-hamburger.svg";
+import close from "../../images/icon-close.svg";
+import "./Header.css";
 
 function Header() {
-
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const bgUrl = window.innerWidth >= 600 ? desktopBg : mobileBg
+  const bgUrl = window.innerWidth >= 700 ? desktopBg : mobileBg;
   const [isOpen, setIsOpen] = useState(false);
+  const isDesktop = windowWidth >= 700 ? true : false;
 
   useEffect(() => {
-    const updateBg = () => {
+    const updateWindow = () => {
       setWindowWidth(window.innerWidth);
-    }
+    };
 
-    window.addEventListener("resize", updateBg);
+    window.addEventListener("resize", updateWindow);
 
-    return () => window.removeEventListener("resize", updateBg);
-    },
-  []);
+    return () => window.removeEventListener("resize", updateWindow);
+  }, []);
 
   const handleToggle = () => {
-    setIsOpen(prev => !prev)
-  }
+    setIsOpen((prev) => !prev);
+  };
 
   return (
-    <section className="header__container" style={{backgroundImage: `url(${bgUrl}), var(--gradient-header)`, objectFit: "cover"}}>
-        <div className="header__head">
-          <img src={logo} alt="Blogr logo" />
-          {!isOpen && 
+    <section
+      className="header__container"
+      style={{
+        backgroundImage: `url(${bgUrl}), var(--gradient-header)`,
+        objectFit: "cover",
+      }}
+    >
+      <div className="header__head">
+        <img className="header__logo" src={logo} alt="Blogr logo" />
+        {!isOpen && !isDesktop && (
           <button className="navbar__btn">
-            <img 
-              src={hamburger} 
-              alt="Click to open menu" 
+            <img
+              src={hamburger}
+              alt="Click to open menu"
               onClick={handleToggle}
             />
-            </button>}
-          {isOpen && 
+          </button>
+        )}
+        {isOpen && !isDesktop && (
           <button className="navbar__btn">
-            <img 
-              src={close} 
-              alt="Click to close menu" 
-              onClick={handleToggle}
-            />
-            </button>}
-        </div>
-        {isOpen && <Navbar />}
-        <h1 className="header__h1">
-          A modern publishing platform
-        </h1>
-        <h2 className="header__h2">
-          Grow your audience and build your online brand
-        </h2>
-        <div className="btn__container">
-            <button className="btn btn--filled">Start for Free</button>
-            <button className="btn btn--outline">Learn More</button>
-        </div>
+            <img src={close} alt="Click to close menu" onClick={handleToggle} />
+          </button>
+        )}
+        {isDesktop && <Navbar desktop={isDesktop}/> }
+      </div>
+      {isOpen && !isDesktop && <Navbar />}
+      <h1 className="header__h1">A modern publishing platform</h1>
+      <h2 className="header__h2">
+        Grow your audience and build your online brand
+      </h2>
+      <div className="btn__container">
+        <button className="header__btn btn btn--filled">Start for Free</button>
+        <button className="header__btn btn btn--outline">Learn More</button>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Header;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,94 +1,129 @@
-import React, {useState} from 'react'
-import upArrow from '../../images/icon-arrow-dark.svg'
-import { navbarLinks, navbarProducts, navbarContact, navbarConnect } from '../../data';
-import './Navbar.css';
+import React, { useState } from "react";
+import upArrow from "../../images/icon-arrow-dark.svg";
+import upArrowDesktop from "../../images/icon-arrow-light.svg";
+import { navbarProducts, navbarContact, navbarConnect } from "../../data";
+import "./Navbar.css";
 
-function Navbar() {
-
-  const [productIsOpen, setProductIsOpen] = useState(false)
-  const [companyIsOpen, setCompanyIsOpen] = useState(false)
-  const [connectIsOpen, setConnectIsOpen] = useState(false)
+function Navbar({ desktop }) {
+  const arrow = window.innerWidth >= 700 ? upArrowDesktop : upArrow;
+  const [productIsOpen, setProductIsOpen] = useState(false);
+  const [companyIsOpen, setCompanyIsOpen] = useState(false);
+  const [connectIsOpen, setConnectIsOpen] = useState(false);
 
   const handleProductToggle = () => {
-    setProductIsOpen(prev => !prev)
-  }
+    setProductIsOpen((prev) => !prev);
+    setCompanyIsOpen(false);
+    setConnectIsOpen(false);
+  };
 
   const handleCompanyToggle = () => {
-    setCompanyIsOpen(prev => !prev)
-  }
+    setCompanyIsOpen((prev) => !prev);
+    setProductIsOpen(false);
+    setConnectIsOpen(false);
+  };
 
   const handleConnectToggle = () => {
-    setConnectIsOpen(prev => !prev)
-  }
+    setConnectIsOpen((prev) => !prev);
+    setProductIsOpen(false);
+    setCompanyIsOpen(false);
+  };
 
   return (
-  <div className='navbar'>
-    {navbarProducts.map((item) => (
-      <nav className="navbar__nav">
-        <button className={productIsOpen ? "navbar__btn navbar__btn--active" : "navbar__btn"} onClick={handleProductToggle}>
-          {item.category}
-            <img className={productIsOpen ? "navbar__arrow--reverse" : "navbar__arrow"} src={upArrow} alt="click to open links for this category" />
-        </button>
-        {productIsOpen &&
-        <ul className="navbar__section">
-         {item.links.map((link) => (
-          <li className="navbar__link">{link}</li>
+    <div className="navbar">
+      <div className="navbar__nav">
+        {navbarProducts.map((item) => (
+          <nav className="navbar__item">
+            <button
+              className={
+                productIsOpen
+                  ? "navbar__btn navbar__btn--active"
+                  : "navbar__btn"
+              }
+              onClick={handleProductToggle}
+            >
+              {item.category}
+              <img
+                className={
+                  productIsOpen ? "navbar__arrow--reverse" : "navbar__arrow"
+                }
+                src={arrow}
+                alt="click to open links for this category"
+              />
+            </button>
+            {productIsOpen && (
+              <ul className="navbar__section">
+                {item.links.map((link) => (
+                  <li className="navbar__link">{link}</li>
+                ))}
+              </ul>
+            )}
+          </nav>
         ))}
-        </ul>
-        }
-      </nav>
-    ))}
-    {navbarContact.map((item) => (
-      <nav className="navbar__nav">
-        <button className={companyIsOpen ? "navbar__btn navbar__btn--active" : "navbar__btn"} onClick={handleCompanyToggle}>
-          {item.category}
-          <img className={companyIsOpen ? "navbar__arrow--reverse" : "navbar__arrow"} src={upArrow} alt="click to open links for this category" />
-      </button>
-      {companyIsOpen &&
-        <ul className="navbar__section">
-         {item.links.map((link) => (
-          <li className="navbar__link">{link}</li>
+        {navbarContact.map((item) => (
+          <nav className="navbar__item">
+            <button
+              className={
+                companyIsOpen
+                  ? "navbar__btn navbar__btn--active"
+                  : "navbar__btn"
+              }
+              onClick={handleCompanyToggle}
+            >
+              {item.category}
+              <img
+                className={
+                  companyIsOpen ? "navbar__arrow--reverse" : "navbar__arrow"
+                }
+                src={arrow}
+                alt="click to open links for this category"
+              />
+            </button>
+            {companyIsOpen && (
+              <ul className="navbar__section">
+                {item.links.map((link) => (
+                  <li className="navbar__link">{link}</li>
+                ))}
+              </ul>
+            )}
+          </nav>
         ))}
-        </ul>
-        }
-      </nav>
-    ))}
 
-{navbarConnect.map((item) => (
-      <nav className="navbar__nav">
-        <button className={connectIsOpen ? "navbar__btn navbar__btn--active" : "navbar__btn"} onClick={handleConnectToggle}>
-          {item.category}
-          <img className={connectIsOpen ? "navbar__arrow--reverse" : "navbar__arrow"} src={upArrow} alt="click to open links for this category" />
-      </button>
-        {connectIsOpen &&
-        <ul className="navbar__section">
-         {item.links.map((link) => (
-          <li className="navbar__link">{link}</li>
+        {navbarConnect.map((item) => (
+          <nav className="navbar__item">
+            <button
+              className={
+                connectIsOpen
+                  ? "navbar__btn navbar__btn--active"
+                  : "navbar__btn"
+              }
+              onClick={handleConnectToggle}
+            >
+              {item.category}
+              <img
+                className={
+                  connectIsOpen ? "navbar__arrow--reverse" : "navbar__arrow"
+                }
+                src={arrow}
+                alt="click to open links for this category"
+              />
+            </button>
+            {connectIsOpen && (
+              <ul className="navbar__section">
+                {item.links.map((link) => (
+                  <li className="navbar__link">{link}</li>
+                ))}
+              </ul>
+            )}
+          </nav>
         ))}
-        </ul>
-        }
-      </nav>
-    ))}
-    {/* {navbarLinks.map((item) => (
-      <ul className="navbar__section">
-        {item.category}
-        <img className="navbar__arrow--dark" src={upArrow} alt="click to open links for this category" />
-        {item.links.map((link) => (
-          <li className='navbar__link'>{link}</li>
-        ))}
-      </ul>
-    ))} */}
-    <div className="navbar__divider"></div>
-    <div className="navbar__btn-container">
-      <button className='btn btn--borderless'>
-        Login
-      </button>
-      <button className='btn btn--gradient'>
-        Sign Up
-      </button>
+      </div>
+      {!desktop && <div className="navbar__divider"></div>}
+      <div className="navbar__btn__container">
+        <button className="navbar__btn btn btn--borderless">Login</button>
+        <button className="navbar__btn btn btn--gradient">Sign Up</button>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Navbar;
